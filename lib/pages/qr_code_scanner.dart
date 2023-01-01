24af240ffdad9cfa458e916710ded61d4ddbe943
Konrad Strenge <koncube@web.de>
2023-01-01 04:43:47 +0100

    qr code scanner alpha

---
+++ lib/pages/qr_code_scanner.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRCodeScanner extends StatefulWidget {
+  const QRCodeScanner({Key? key}) : super(key: key);
+
+  @override
+  State<QRCodeScanner> createState() => _QRCodeScannerState();
+}
+
+class _QRCodeScannerState extends State<QRCodeScanner> {
+  final GlobalKey qrKey = GlobalKey();
+  QRViewController? controller;
+  Barcode? result;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
