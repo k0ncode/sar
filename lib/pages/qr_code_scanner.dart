818fb0db46682a7fea556f4c2b7f644660ffd6e4
k0ncode <67321116+k0ncode@users.noreply.github.com>
2023-01-11 20:28:06 +0100

    working qrcode scanner with url launcher

--- lib/pages/qr_code_scanner.dart
+++ lib/pages/qr_code_scanner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QRCodeScanner extends StatefulWidget {
   const QRCodeScanner({Key? key}) : super(key: key);
@@ -14,19 +15,53 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("QR-Code Scanner")),
-      body: MobileScanner(
-        allowDuplicates: false,
-        controller: cameraController,
-        onDetect: (barcode, args) {
-          if (barcode.rawValue == null) {
-            debugPrint("Failed to scan QR-Code");
-          } else {
-            final String code = barcode.rawValue!;
-            debugPrint("QR-Code found");
-          }
-        },
+      appBar: AppBar(
+        title: const Text("QR-Code Scanner"),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_on);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_off);
+                }
+              },
+            ),
+            onPressed: () => cameraController.toggleTorch(),
+          )
+        ],
       ),
+      body: Stack(children: [
+        MobileScanner(
+          allowDuplicates: false,
+          controller: cameraController,
+          onDetect: (qrcode, args) async {
+            if (qrcode.rawValue == null) {
+              debugPrint("Failed to scan QR-Code");
+            } else {
+              final String qrCodeValue = qrcode.rawValue!;
+              final Uri url = Uri.parse(qrCodeValue);
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url, mode: LaunchMode.externalApplication);
+              } else {
+                debugPrint("$url kann nicht geöffnet werden");
+              }
+              debugPrint("QR-Code found: $qrCodeValue");
+            }
+          },
+        ),
+        Center(
+          child: Container(
+            height: 180,
+            width: 180,
+            decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 5)),
+          ),
+        ),
+      ]),
     );
   }
 }
